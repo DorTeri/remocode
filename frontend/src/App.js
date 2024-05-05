fcb6655b419db9f8897e2647aeb9cf4497637f22
DorTeri <dortayari@gmail.com>
2024-05-05 19:10:06 +0300

    finished store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,19 @@ import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import AppHeader from './cmps/AppHeader';
 import HomePage from './views/HomePage';
 import CodeBlockDetails from './views/CodeBlockDetails';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getAllCodeBlocks } from './store/actions/codeBlocks.actions';
 
 
 function App() {
+  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllCodeBlocks())
+  }, [dispatch])
+  
   return (
     <Router>
       <section className='main-app main-layout'>
--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -1,36 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import CodeBlockPreview from '../cmps/CodeBlockPreview'
-import { codeBlocks } from '../constants'
-import { codeBlockService } from '../services/codeBlocks.service'
+import { useSelector } from 'react-redux'
 
 const HomePage = () => {
 
-  const [codeBlocksFromBack, setCodeBlocksFromBack] = useState(null)
+  const codeBlocks = useSelector((storeState) => storeState.codeBlocksModule.codeBlocks)
 
-  useEffect(() => {
-    fetchCodeBlocks()
-  }, [])
-
-  const fetchCodeBlocks = async () => {
-    const codeBlocks = await codeBlockService.getAllCodeBlocks()
-    setCodeBlocksFromBack(codeBlocks)
-  }
-
-
+  if (!codeBlocks) return (
+    <div>
+      Loading..
+    </div>
+  )
 
   return (
     <section className='homepage-container'>
       <h2>Choose Code Block</h2>
       <div className='code-blocks-container'>
         {
-          codeBlocksFromBack ? (
-            codeBlocksFromBack.map(code => (
-              <CodeBlockPreview code={code} key={code.title} />
-            ))) : (
-            codeBlocks.map(code => (
-              <CodeBlockPreview code={code} key={code.title} />
-            ))
-          )
+          codeBlocks.map(code => (
+            <CodeBlockPreview code={code} key={code.title} />
+          ))
         }
       </div>
     </section>
