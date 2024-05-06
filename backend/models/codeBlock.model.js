9a9017c4b623966169d52fd113c822740c176fed
DorTeri <dortayari@gmail.com>
2024-05-07 00:13:37 +0300

    Added solution required to schema

--- backend/models/codeBlock.model.js
+++ backend/models/codeBlock.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const codeBlockSchema = new mongoose.Schema({
   title: { type: String, required: true },
   code: { type: String, required: true },
-  solution: {type: String},
+  solution: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 }, { collection: 'codeBlocks' });
 
