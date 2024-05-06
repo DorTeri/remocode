6194309b20365a6801ad3498187f0d1baaeb487e
DorTeri <dortayari@gmail.com>
2024-05-06 22:09:01 +0300

    Changed port

--- backend/server.js
+++ backend/server.js
@@ -36,13 +36,14 @@ mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use('/codeBlock', codeBlockRoutes);
 
-app.use('/', (req, res) => {
-    res.send("Server is running")
-})
 
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
+
+app.use('/', (req, res) => {
+    res.send(`Server is running ${port}`)
+})
 
 
 http.listen(port, () => {
